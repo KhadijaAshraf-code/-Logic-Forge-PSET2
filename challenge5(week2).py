eea4e6e9d3434229df00d832f5b70d17a5fffaae
KhadijaAshraf-code <khadijaashrafali@gmail.com>
2026-01-12 17:39:58 +0500

    Add files via upload

---
+++ challenge5(week2).py
@@ -0,0 +1,21 @@
+def min_cancelled_bookings(intervals):
+    if not intervals:
+        return 0
+
+    # Sort by end time
+    intervals.sort(key=lambda x: x[1])
+
+    removals = 0
+    prev_end = intervals[0][1]
+
+    for i in range(1, len(intervals)):
+        start, end = intervals[i]
+
+        if start < prev_end:
+            # Overlap → remove this meeting
+            removals += 1
+        else:
+            # No overlap → accept
+            prev_end = end
+
+    return removals
