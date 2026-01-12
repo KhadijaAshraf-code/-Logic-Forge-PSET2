3d799bb26039069a0d37bb151416a7e8c3a3c90b
KhadijaAshraf-code <khadijaashrafali@gmail.com>
2026-01-12 17:58:59 +0500

    Add files via upload

---
+++ challenge6(week2)final.py
@@ -0,0 +1,23 @@
+def maximize_freelance_profit(deadlines, profits):
+    jobs = list(zip(deadlines, profits))
+    jobs.sort(key=lambda x: x[1], reverse=True)  # sort by profit
+
+    max_deadline = max(deadlines)
+    slots = [-1] * (max_deadline + 1)
+
+    job_count = 0
+    total_profit = 0
+
+    for deadline, profit in jobs:
+        for t in range(deadline, 0, -1):
+            if slots[t] == -1:
+                slots[t] = profit
+                job_count += 1
+                total_profit += profit
+                break
+
+    return [job_count, total_profit]
+print(maximize_freelance_profit([4,1,1,1], [20,10,40,30]))
+# Output: [2, 60]
+
+
