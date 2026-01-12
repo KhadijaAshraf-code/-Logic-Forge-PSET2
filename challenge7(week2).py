2c7b8c09442144dc733c4a976790f59445c923af
KhadijaAshraf-code <khadijaashrafali@gmail.com>
2026-01-12 18:05:54 +0500

    Add files via upload

---
+++ challenge7(week2).py
@@ -0,0 +1,17 @@
+import math
+
+def calculate_minimum_speed(piles, k):
+    max_pile = max(piles)
+
+    for speed in range(1, max_pile + 1):
+        hours = 0
+        for pile in piles:
+            hours += math.ceil(pile / speed)
+
+        if hours <= k:
+            return speed
+
+print(calculate_minimum_speed([5,10,3], 4))
+# Output: 5
+
+
